272105debf51cb24697f41c94f3f0685298f0c59
kekewei <keke.wei@cs2c.com.cn>
2014-09-05 15:10:47 +0800

    修改了getClusterInfo方法，补充了getClusterNameById方法 新添了集群网络相关的5个方法：getClusterNetworkList、getNetworkInfo、attachNetworkTocluster、detachNetworkFromcluster、updateNetworkOfCluster

--- src/TestAPIs/ClusterAPIs.py
+++ src/TestAPIs/ClusterAPIs.py
@@ -1,4 +1,6 @@
 #encoding:utf-8
+from TestAPIs.NetworkAPIs import NetworkAPIs
+from TestAPIs.DataCenterAPIs import DataCenterAPIs
 
 __authors__ = ['"keke wei" <keke.wei@cs2c.com.cn>']
 __version__ = "V0.1"
@@ -45,6 +47,18 @@ class ClusterAPIs(BaseAPIs):
         '''
         cluster_list = self.searchClusterByName(cluster_name)
         return cluster_list['result']['clusters']['cluster']['@id']
+    
+    def getClusterNameById(self, cluster_id):
+        '''
+        @summary: 根据集群id获取名称
+        @param dc_id: 集群id
+        @return: 集群名称
+        '''
+        api_url = '%s/%s' % (self.base_url, cluster_id)
+        method = 'GET'
+        r = HttpClient.sendRequest(method=method, api_url=api_url)
+        if r.status_code==200:
+            return xmltodict.parse(r.text)['cluster']['name']
         
     def getClustersList(self):
         '''
@@ -56,13 +70,14 @@ class ClusterAPIs(BaseAPIs):
         r = HttpClient.sendRequest(method=method, api_url=api_url)
         return {'status_code':r.status_code, 'result':xmltodict.parse(r.text)} 
     
-    def getClusterInfo(self, cluster_name):
+    def getClusterInfo(self, cluster_name=None, cluster_id=None):
         '''
         @summary: 根据集群名称，获取集群详细信息
         @param cluster_name: 集群名称
         @return: 字典：（1）status_code：请求返回码；（2）result：dict形式的数据中心信息
         '''
-        cluster_id = self.getClusterIdByName(cluster_name)
+        if not cluster_id and cluster_name:
+            cluster_id = self.getClusterIdByName(cluster_name)
         api_url = '%s/%s' % (self.base_url, cluster_id)
         method = 'GET'
         r = HttpClient.sendRequest(method=method, api_url=api_url)
@@ -130,13 +145,63 @@ class ClusterAPIs(BaseAPIs):
         for network in network_list:
             if network['name']==network_name:
                 return network
+            
            
+    def attachNetworkToCluster(self, cluster_name, nw_info=None):
+        '''
+        @summary: 将网络附加到集群
+        @param cluster_name: 集群名称
+        @param nw_info: 网络名称或网络id
+        @return: 字典，（1）status_code：请求返回状态码；（2）result：请求返回的内容。
+        '''
+        api_url = '%s/%s/networks' % (self.base_url, self.getClusterIdByName(cluster_name))
+        method = 'POST'
+        r = HttpClient.sendRequest(method=method, api_url=api_url, data=nw_info)
+        return {'status_code':r.status_code, 'result':xmltodict.parse(r.text)}
+    
+    def detachNetworkFromCluster(self, cluster_name, nw_name, async=None):
+        '''
+        @summary: 将附加到集群的网络进行分离
+        @param cluster_name: 集群名称
+        @param nw_name: 网络名称
+        @param data: 可选参数，如<action><async>true</async></action>
+        @return: 字典，（1）status_code：请求返回状态码；（2）result：请求返回的内容。
+        '''
+        cluster_id = self.getClusterIdByName(cluster_name)
+        dc_id = self.getClusterInfo(cluster_name)['result']['cluster']['data_center']['@id']
+        print dc_id
+        dc_name = DataCenterAPIs().getDataCenterNameById(dc_id)
+        nw_id = NetworkAPIs().getNetworkIdByName(nw_name, dc_name)
+        method = 'DELETE'
+        api_url = '%s/%s/networks/%s' % (self.base_url, cluster_id, nw_id)
+        r = HttpClient.sendRequest(method=method, api_url=api_url)
+        return {'status_code':r.status_code, 'result':xmltodict.parse(r.text)}
+    
+    def updateNetworkOfCluster(self, cluster_name, nw_name,data=None):
+        '''
+        @summary: 更新附加到集群的网络信息
+        @param cluster_name: 集群名称
+        @param nw_name: 网络名称
+        @param data:更新的信息
+        @return: 字典，（1）status_code：请求返回状态码；（2）result：请求返回的内容。
+        '''
+        cluster_id = self.getClusterIdByName(cluster_name)
+        dc_id = self.getClusterInfo(cluster_name)['result']['cluster']['data_center']['@id']
+        dc_name = DataCenterAPIs().getDataCenterNameById(dc_id)
+        nw_id = NetworkAPIs().getNetworkIdByName(nw_name, dc_name)
+        method = 'PUT'
+        api_url = '%s/%s/networks/%s' % (self.base_url, cluster_id, nw_id)
+        r = HttpClient.sendRequest(method=method, api_url=api_url,data=data)
+        return {'status_code':r.status_code, 'result':xmltodict.parse(r.text)}
+    
 if __name__=='__main__':
     clusterapi = ClusterAPIs()
     #print clusterapi.searchClusterByName('Default')
     #print clusterapi.getClusterIdByName('Default')
+    #print clusterapi.getClusterNameById('46951ef6-5bdb-4da3-89e0-092782b35487')
     #print clusterapi.getClustersList()
     #print clusterapi.getClusterInfo('Default')
+    #print clusterapi.getClusterInfo(cluster_id='46951ef6-5bdb-4da3-89e0-092782b35487')
     data = '''
     <cluster>
         <name>NewCluster1</name>
@@ -153,8 +218,34 @@ if __name__=='__main__':
     '''
     #print clusterapi.delCluster('NewCluster1',data1)
     #print clusterapi.getClusterNetworkList('Default')
-    print clusterapi.getClusterNetworkInfo('Default','aaa')
-    #test
+    #print clusterapi.getClusterNetworkInfo('Default','aaa')
+    data2 = '''
+    <network id="ef00d7c4-7d9a-4c3b-934c-b1ac7298eaf1">    
+    </network>
+    '''
+    data3 = '''
+    <network>
+       <name>network11</name>
+    </network>
+    '''
+   
+    #print clusterapi.attachNetworkToCluster('Default', data2)
+    #print clusterapi.attachNetworkToCluster('Default', data3)
+    
+    print clusterapi.detachNetworkFromCluster('Default','aaa')
+    data4 = '''
+    <network>
+    <display>false</display>
+    <usages>
+        <usage>VM</usage>
+        <usage>DISPLAY</usage>
+    </usages>
+    </network>
+    '''
+    #print clusterapi.updateNetworkOfCluster('Default', 'aaa', data4)
+    print clusterapi.updateNetworkOfCluster('Default', 'network11', data4)
+
+   
 
     
     
