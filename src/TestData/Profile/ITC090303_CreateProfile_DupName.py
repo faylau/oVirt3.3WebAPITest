640e45190e0c83f29b30a1cc21b2da390cc17eda
faylau <fei.liu@cs2c.com.cn>
2014-11-19 11:00:26 +0800

    修改了Profile模块中的用例、数据文件编号
    1.修改了Profile模块中的用例、数据文件编号；
    2.为数据文件及TC文件添加了版本信息。

---
+++ src/TestData/Profile/ITC090303_CreateProfile_DupName.py
@@ -0,0 +1,52 @@
+#encoding:utf-8
+
+__authors__ = ['"Wei Keke" <keke.wei@cs2c.com.cn>']
+__version__ = "V0.1"
+
+'''
+# ChangeLog:
+#---------------------------------------------------------------------------------
+# Version        Date                Desc                            Author
+#---------------------------------------------------------------------------------
+# V0.1           2014/10/09          初始版本                                                            Wei Keke 
+#---------------------------------------------------------------------------------
+'''
+
+from TestAPIs.DataCenterAPIs import DataCenterAPIs
+from TestData.Profile import ITC09_SetUp as ModuleData
+
+'''
+@note: PreData
+'''
+nw_name = 'network_ITC09'
+dc_name = ModuleData.dc_name
+dc_id = DataCenterAPIs().getDataCenterIdByName(dc_name)
+profile_name = 'profile_ITC09'
+nw_info = '''
+<network>
+    <name>%s</name>
+    <data_center id= "%s"/>    
+</network>
+''' %(nw_name, dc_id)
+
+# 字符串中的network id是通过外部调用的函数传递的，不在字符串中传递。
+profile_info = '''
+    <vnic_profile>
+        <name>profile_ITC09</name>
+        <description>shelled</description>
+        <network id="%s"/>
+        <port_mirroring>false</port_mirroring>
+    </vnic_profile>
+'''
+'''
+@note: ExpectedData
+'''
+expected_status_code = 409
+expected_info = ''' 
+<fault>
+    <reason>Operation Failed</reason>
+    <detail>[Cannot add VM network interface profile. The VM network interface profile's name is already used by an existing profile for the same network.
+-Please choose a different name.]</detail>
+</fault>
+'''
+
--- src/TestData/Profile/ITC090402_UpdateProfile_DupName.py
+++ src/TestData/Profile/ITC090402_UpdateProfile_DupName.py
@@ -1,12 +1,24 @@
 #encoding:utf-8
 
+__authors__ = ['"Wei Keke" <keke.wei@cs2c.com.cn>']
+__version__ = "V0.1"
+
+'''
+# ChangeLog:
+#---------------------------------------------------------------------------------
+# Version        Date                Desc                            Author
+#---------------------------------------------------------------------------------
+# V0.1           2014/10/09          初始版本                                                            Wei Keke 
+#---------------------------------------------------------------------------------
+'''
+
 from TestAPIs.DataCenterAPIs import DataCenterAPIs
-from TestData.Profile import ITC06_SetUp as ModuleData
+from TestData.Profile import ITC09_SetUp as ModuleData
 
 '''
 @note: PreData
 '''
-nw_name = 'network_ITC06'
+nw_name = 'network_ITC09'
 dc_name = ModuleData.dc_name
 dc_id = DataCenterAPIs().getDataCenterIdByName(dc_name)
 nw_info = '''
@@ -14,7 +26,7 @@ nw_info = '''
     <name>%s</name>
     <data_center id= "%s"/>    
 </network>
-''' %(nw_name,dc_id)
+''' %(nw_name, dc_id)
 
 
 profile_name1 = 'p001'
